bf4f684bf93c36898811e3e446a88d93a235f9a6
sbx_user1051 <sbx_user1051@169.254.21.101>
2022-09-17 18:09:19 +0000

    Sat Sep 17 18:09:19 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,27 +2,19 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         Integer total = 0, aItems = 0, bItems = 0, cItems = 0, dItems = 0, eItems = 0, fItems = 0, discount = 0;
-        String[] products = new String[20];
+        List<String> products = new ArrayList<>();
         Integer[] items = new Integer[20];
         for(int i = 0; i < skus.length(); i++){
             char product = skus.charAt(i);
-            if(product == 'A'){
-                aItems+=1;
-            }else if(product == 'B'){
-                bItems+=1;
-            }else if(product == 'C'){
-                cItems+=1;
-            }else if(product == 'D'){
-                dItems+=1;
-            }else if(product == 'E'){
-                eItems+=1;
-            }else if(product == 'F'){
-                fItems+=1;
-            }else{
-                return -1;
+            if(!products.contais(product)){
+
+
             }
         }
         //discount rules
@@ -34,3 +26,4 @@ public class CheckoutSolution {
         return total;
     }
 }
+
