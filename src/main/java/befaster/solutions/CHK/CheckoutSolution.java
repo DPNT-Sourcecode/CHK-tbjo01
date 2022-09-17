4a7b1b6076b2520b8d7272fe169058899fe53f5e
sbx_user1051 <sbx_user1051@169.254.21.101>
2022-09-17 18:15:19 +0000

    Sat Sep 17 18:15:19 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        Integer total = 0, aItems = 0, bItems = 0, cItems = 0, dItems = 0, eItems = 0, fItems = 0, discount = 0;
+        Integer total = 0, discount = 0;
         List<String> products = new ArrayList<String>();
-        Integer[] items = new Integer[20];
+        List<Integer> items = new ArrayList<Integer>();
+        List<Integer> productPrices = new ArrayList<Integer>();
         for(int i = 0; i < skus.length(); i++){
             char product = skus.charAt(i);
             if(!products.contains(Character.toString(product))){
@@ -17,20 +18,25 @@ public class CheckoutSolution {
             }
         }
         for(String product : products){
-            qtdItems = 0;
+            Integer qtdItems = 0;
             for(int i = 0; i < skus.length(); i++){
-                char prdct = skus.charAt(i);
-                
+                char currentProduct = skus.charAt(i);
+                if(Character.toString(currentProduct).equals(product)){
+                    qtdItems+=1;
+                }
             }
+            items.add(qtdItems);
         }
         //discount rules
-        if(bItems >= eItems/2) bItems = bItems-eItems/2;
-        if(fItems >= 3) fItems -= fItems/3;
-        total = aItems*50 + bItems*30 + cItems*20 + dItems*15 + eItems*40 - (bItems/2)*15 + fItems*10;
-        discount = aItems/5*50 + ((aItems-((aItems/5)*5))/3)*20;
+        //if(bItems >= eItems/2) bItems = bItems-eItems/2;
+        //if(fItems >= 3) fItems -= fItems/3;
+        //total = aItems*50 + bItems*30 + cItems*20 + dItems*15 + eItems*40 - (bItems/2)*15 + fItems*10;
+        //discount = aItems/5*50 + ((aItems-((aItems/5)*5))/3)*20;
+
         total -= discount;
         return total;
     }
 }
+
 
 
